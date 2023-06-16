8a7318673131dec3ad4638c3204347add75e6b63
mahmoud atef <mahmoud.atef3167@gmail.com>
2023-06-16 14:32:21 +0200

    theme completed 1

--- lib/core/utils/theme_manager.dart
+++ lib/core/utils/theme_manager.dart
@@ -7,78 +7,99 @@ import 'styles_manager.dart';
 
 ThemeData getAppTheme() {
   return ThemeData (
-      fontFamily: 'Abg',
-      hintColor: ColorManager.primary,
-      hoverColor: ColorManager.primary.withOpacity(.10),
-     cardColor: ColorManager.primary,
+    appBarTheme: const AppBarTheme(
+      foregroundColor: Color(0xFFfffafafb),
+    ),
+    applyElevationOverlayColor: false,
+    brightness: Brightness.light,
+    buttonTheme: const ButtonThemeData(
+      alignedDropdown: false,
       colorScheme: ColorScheme(
-          brightness: Brightness.light,
-          primary: ColorManager.white,
-          onPrimary: ColorManager.black,
-          secondary: ColorManager.secondary,
-          onSecondary: ColorManager.white,
-          error: ColorManager.error,
-          onError: ColorManager.error,
-          background: ColorManager.white,
-          onBackground: ColorManager.white,
-          surface: ColorManager.white,
-          onSurface: ColorManager.black),
-      primarySwatch: const MaterialColor(4280765594,{50: Color( 0xffebf0fa )
-        , 100: Color( 0xffd6e0f5 )
-        , 200: Color( 0xffaec1ea )
-        , 300: Color( 0xff85a2e0 )
-        , 400: Color( 0xff5c83d6 )
-        , 500: Color( 0xff3464cb )
-        , 600: Color( 0xff2950a3 )
-        , 700: Color( 0xff1f3c7a )
-        , 800: Color( 0xff152851 )
-        , 900: Color( 0xff0a1429 )
-      }),
-    drawerTheme: DrawerThemeData(
-      width: 70.w ,
+        background: Color(0xFFff6882b8),
+        brightness: Brightness.light,
+        error: Color(0xFFffb00020),
+        errorContainer: Color(0xFFffb00020),
+        inversePrimary: Color(0xFFffffffff),
+        inverseSurface: Color(0xFF000000),
+        onBackground: Color(0xFF000000),
+        onError: Color(0xFFffffffff),
+        onErrorContainer: Color(0xFFffffffff),
+        onInverseSurface: Color(0xFFffffffff),
+        onPrimary: Color(0xFF000000),
+        onPrimaryContainer: Color(0xFFffffffff),
+        onSecondary: Color(0xFF000000),
+        onSecondaryContainer: Color(0xFF000000),
+        onSurface: Color(0xFF000000),
+        onSurfaceVariant: Color(0xFF000000),
+        onTertiary: Color(0xFF000000),
+        onTertiaryContainer: Color(0xFF000000),
+        outline: Color(0xFF000000),
+        outlineVariant: Color(0xFF000000),
+        primary: Color(0xFF274c9a),
+        primaryContainer: Color(0xFF6200ee),
+        secondary: Color(0xFF5db134),
+        secondaryContainer: Color(0xFF03dac6),
+        shadow: Color(0xFF000000),
+        surface: Color(0xFFFFFFFF),
+        surfaceTint: Color(0xFF6200ee),
+        surfaceVariant: Color(0xFFFFFFFF),
+        tertiary: Color(0xFF03dac6),
+        tertiaryContainer: Color(0xFF03dac6),
+      ),
+      height: 36,
+
+      minWidth: 88,
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(2),
+            bottomRight: Radius.circular(2),
+            topLeft: Radius.circular(2),
+            topRight: Radius.circular(2),
+          )
+      ),
+      textTheme: ButtonTextTheme.normal,
+    ),
+    canvasColor: const Color(0xFFfafafa),
+    cardColor: const Color(0xFFFFFFFF),
+    dialogBackgroundColor: const Color(0xFFFFFFFF),
+    disabledColor: const Color(0xFF61000000),
+    dividerColor: const Color(0xFF1f000000),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(/* ... */),
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      foregroundColor: Color(0xFFfffafafb),
+    ),
+    focusColor: const Color(0xFF1f000000),
+    highlightColor: const Color(0xFF66bcbcbc),
+    hintColor: const Color(0xFF99000000),
+    hoverColor: const Color(0xFF0a000000),
+    iconTheme: const IconThemeData(
+      color: Color(0xFFfffafafb),
+    ),
+    indicatorColor: const Color(0xFF274c9a),
+    inputDecorationTheme: const InputDecorationTheme(/* ... */),
+    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+    platform: TargetPlatform.windows,
+    primaryColor: const Color(0xFF274c9a),
+    primaryColorDark: const Color(0xFF1f3d7b),
+    primaryColorLight: const Color(0xFF7d94c2),
+    primaryIconTheme:  IconThemeData(
+       color: ColorManager.white
+    ),
+    scaffoldBackgroundColor: const Color(0xFFfafafa),
+    secondaryHeaderColor: const Color(0xFF889dc7),
+    shadowColor: const Color(0xFF000000),
+    splashColor: const Color(0xFF66c8c8c8),
+    splashFactory: InkSplash.splashFactory,
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(/* ... */),
     ),
 
-      textButtonTheme: const TextButtonThemeData(
-      ),
-      scaffoldBackgroundColor: ColorManager.white,
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.white,
-      disabledColor: ColorManager.grey1,
-      appBarTheme: AppBarTheme(
-        color: ColorManager.white,
-        elevation: 0,
-        centerTitle: false,
-        titleSpacing: 1.2.sp,
-        titleTextStyle: getBoldStyle(
-          color: ColorManager.black,
-          fontSize: FontSizeManager.s16.sp,
-        ),
-        iconTheme:
-        IconThemeData(color: ColorManager.black, size: 25.sp),
-      ),
-      floatingActionButtonTheme: FloatingActionButtonThemeData(backgroundColor: ColorManager.primary, ),
-      buttonTheme: ButtonThemeData(
-        //  disabledColor: ColorManager.grey1,
-        buttonColor: ColorManager.primary,
-        height: SizeManager.s50.sp,
-        colorScheme: ColorScheme.fromSeed(seedColor: ColorManager.primary, background: ColorManager.primary, ),
-
-        shape:  const StadiumBorder(),
-        //textTheme: ButtonTextTheme.primary,
-      ),
-      cardTheme: CardTheme(
-        color: ColorManager.white,
-        elevation: SizeManager.s4.sp,
-        clipBehavior: Clip.antiAliasWithSaveLayer,
-      ),
-      unselectedWidgetColor: ColorManager.grey1,
-      elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              textStyle: getRegularStyle(
-                  color: ColorManager.white, fontSize: FontSizeManager.s16.sp),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(SizeManager.s15.sp)))),
-      iconTheme: IconThemeData(color: ColorManager.primary)
-    // label style
+    typography: Typography(/* ... */),
+    unselectedWidgetColor: const Color(0xFF8a0a0a0a),
+    useMaterial3: false,
+    visualDensity: VisualDensity.compact,
   );
 }
